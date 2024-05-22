7e9420d506c5998680665ce0fba1057205efa532
wael <78425538+wa20@users.noreply.github.com>
2024-05-22 18:36:19 +0100

    installed clerk and added middleware, also wraped parent to create protected routes

--- linked/app/layout.tsx
+++ linked/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+     <html lang="en">
       <body className="min-h-screen flex flex-col ">
         {/* TODO Toaster */}
         
@@ -27,6 +29,7 @@ export default function RootLayout({
           {children}
         </main>
       </body>
-    </html>
+    </html> 
+    </ClerkProvider>
   );
 }
