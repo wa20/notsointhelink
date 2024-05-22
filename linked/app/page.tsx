d353e22c9a4c655a9157caf0a3cad93112c2e41a
wael <78425538+wa20@users.noreply.github.com>
2024-05-22 22:43:51 +0100

    added styling to page file

--- linked/app/page.tsx
+++ linked/app/page.tsx
@@ -3,12 +3,12 @@ import UserInfo from "@/components/UserInfo";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-8 mt-5 sm:px-5">
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
         <UserInfo />
       </section>
 
-      <section className="col-span-full md-col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
      <PostForm />
     {/* postFeed */}
 
@@ -17,6 +17,6 @@ export default function Home() {
       <section className="hidden xl:inline justify-center col-span-2">
         {/* widget */}
       </section>
-    </main>
+    </div>
   );
 }
