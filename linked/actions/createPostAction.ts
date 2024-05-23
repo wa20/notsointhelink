49dedfbb270da5d72dde156fc5cb1c203297772f
wael <78425538+wa20@users.noreply.github.com>
2024-05-23 17:50:24 +0100

    updated code in action file to define a user

--- linked/actions/createPostAction.ts
+++ linked/actions/createPostAction.ts
@@ -1,16 +1,19 @@
 'use server'
 
+import { AddPostRequestBody } from "@/app/api/posts/route";
+import { Post } from "@/mongodb/models/post";
+import { IUser } from "@/types/user";
 import { currentUser } from "@clerk/nextjs/server"
 
 export default async function createPostAction(formData: FormData) {
-    
+
     // we can use the below auth().protect() example to protect the route, but will not be able to control the error message
     // auth().protect();
-    
+
     //this checks the current user and if it doesn't exist, throws an error
     const user = await currentUser();
 
-    if(!user) {
+    if (!user) {
         throw new Error('User not found')
     }
 
@@ -22,17 +25,41 @@ export default async function createPostAction(formData: FormData) {
 
 
     // if no post input, throw an error
-    if (!postInput){
+    if (!postInput) {
         throw new Error('Post cannot be empty')
     }
 
     //define user
+    const userDB: IUser = {
+        userId: user.id,
+        userImage: user.imageUrl,
+        firstName: user.firstName || "",
+        lastName: user.lastName || "",
+    }
 
+
+    try {
+        if (image.size > 1) {
+
+            // TODO: upload to storage
+
+        } else {
+
+            const body: AddPostRequestBody = {
+                user: userDB,
+                text: postInput
+            }
+            await Post.create(body)
+        }
+
+    } catch (error: any) {
+        throw new Error('Error creating post', error);
+    }
     // upload image if there is one
 
     //create post in database
 
     // revalidate the path/homepage so that all the data reloads
-    
+
 
 }
