604a60b2dbc130301c4aa178a9b7e088872df89c
wael <78425538+wa20@users.noreply.github.com>
2024-05-23 20:28:38 +0100

    formatting amends

--- linked/actions/createPostAction.ts
+++ linked/actions/createPostAction.ts
@@ -1,15 +1,14 @@
-'use server'
+"use server";
 
 import { AddPostRequestBody } from "@/app/api/posts/route";
 import { Post } from "@/mongodb/models/post";
 import { IUser } from "@/types/user";
-import { currentUser } from "@clerk/nextjs/server"
+import { currentUser } from "@clerk/nextjs/server";
 import { BlobServiceClient } from "@azure/storage-blob";
 import generateSASToken, { containerName } from "@/lib/generateSASToken";
 import { randomUUID } from "crypto";
 
 export default async function createPostAction(formData: FormData) {
-
     // we can use the below auth().protect() example to protect the route, but will not be able to control the error message
     // auth().protect();
 
@@ -17,19 +16,18 @@ export default async function createPostAction(formData: FormData) {
     const user = await currentUser();
 
     if (!user) {
-        throw new Error('User not found')
+        throw new Error("User not found");
     }
 
-    const postInput = formData.get('postInput') as string;
-    const image = formData.get('image') as File;
+    const postInput = formData.get("postInput") as string;
+    const image = formData.get("image") as File;
 
     // we use a let below as value could be either a string or undefined
     let image_url: string | undefined = undefined;
 
-
     // if no post input, throw an error
     if (!postInput) {
-        throw new Error('Post cannot be empty')
+        throw new Error("Post cannot be empty");
     }
 
     //define user
@@ -38,13 +36,11 @@ export default async function createPostAction(formData: FormData) {
         userImage: user.imageUrl,
         firstName: user.firstName || "",
         lastName: user.lastName || "",
-    }
-
+    };
 
     try {
         if (image.size > 1) {
-
-            console.log('image blob uploaded to Azure Storage: ', image)
+            console.log("image blob uploaded to Azure Storage: ", image);
             // TODO: upload to storage
 
             // 1. upload image if it exists to Azure Storage
@@ -55,46 +51,42 @@ export default async function createPostAction(formData: FormData) {
                 `https://${accountName}.blob.core.windows.net${sasToken}`
             );
 
-            const containerClient = blobServiceClient.getContainerClient(containerName);
+            const containerClient =
+                blobServiceClient.getContainerClient(containerName);
 
             const timestamp = new Date().getTime();
-            const file_name = `${randomUUID()}-${timestamp}.png`
+            const file_name = `${randomUUID()}_${timestamp}.png`;
 
             // this will push data to our container
             const blockBlobClient = containerClient.getBlockBlobClient(file_name);
 
             const imageBuffer = await image.arrayBuffer();
             const res = await blockBlobClient.uploadData(imageBuffer);
-            image_url = res._response.request.url
+            image_url = res._response.request.url;
 
-            console.log('image url uploaded: ', image_url)
+            console.log("image url uploaded: ", image_url);
 
             // 2. create a post in the database with the image url
 
             const body: AddPostRequestBody = {
                 user: userDB,
                 text: postInput,
-                imageUrl: image_url
-            }
-            await Post.create(body)
-
+                imageUrl: image_url,
+            };
+            await Post.create(body);
         } else {
-
             const body: AddPostRequestBody = {
                 user: userDB,
-                text: postInput
-            }
-            await Post.create(body)
+                text: postInput,
+            };
+            await Post.create(body);
         }
-
     } catch (error: any) {
-        throw new Error('Error creating post - actions', error);
+        throw new Error("Error creating post - actions", error);
     }
     // upload image if there is one
 
     //create post in database
 
     // revalidate the path/homepage so that all the data reloads
-
-
 }
