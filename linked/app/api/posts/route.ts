e05597ec159a359cf3116fd73aae744907dfeabe
wael <78425538+wa20@users.noreply.github.com>
2024-05-23 17:55:51 +0100

    created api file with posts route folder to handle get and post requests

---
+++ linked/app/api/posts/route.ts
@@ -0,0 +1,50 @@
+import connectDB from "@/mongodb/db";
+import { IUser } from "@/types/user";
+import { IPostBase, Post } from "@/mongodb/models/post";
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs/server";
+
+//type interface
+
+export interface AddPostRequestBody {
+  user: IUser;
+  text: string;
+  imageUrl?: string | null;
+}
+
+export async function POST(request: Request) {
+  auth().protect(); //this protects the route using clerk authentication
+
+  try {
+    await connectDB();
+
+    const { user, text, imageUrl }: AddPostRequestBody = await request.json();
+
+    const postData: IPostBase = {
+      user,
+      text,
+      ...(imageUrl && { imageUrl }),
+    };
+
+    const post = await Post.create(postData);
+    return NextResponse.json({message: "Post created successfully", post }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+        { error: `An error occurred while creating the post, ${error}` },
+        { status: 500 }
+      );
+  }
+} 
+
+export async function GET(request: Request) {
+  try {
+    await connectDB();
+    const posts = await Post.getAllPosts();
+    return NextResponse.json({ posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { error: `An error occurred while fetching posts ${error}` },
+      { status: 500 }
+    );
+  }
+}
