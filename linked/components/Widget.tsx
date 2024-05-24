7f40039561bba173dacd9eac396e793e74cef433
wael <78425538+wa20@users.noreply.github.com>
2024-05-24 17:12:12 +0100

    creaeted weather widget using iframe

---
+++ linked/components/Widget.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Widget() {
+    return (
+        <div className="ml-6 h-[790px]">
+            <iframe
+                src="https://c9825f53c9bf4734b0118d770f0dae6c.elf.site"
+                title="Embedded post"
+                className="w-fit 2xl:min-w-[400px] h-full"
+            >
+            </iframe>
+        </div>
+    );
+  }
+  
+  export default Widget;
