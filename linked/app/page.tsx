d3634a8cd2289a4208a259b6d8c73f9fca23adf4
wael <78425538+wa20@users.noreply.github.com>
2024-05-24 10:12:22 +0100

    added Post form component to main homepage

--- linked/app/page.tsx
+++ linked/app/page.tsx
@@ -1,7 +1,20 @@
+import PostFeed from "@/components/PostFeed";
 import PostForm from "@/components/PostForm";
 import UserInfo from "@/components/UserInfo";
+import connectDB from "@/mongodb/db";
+import { Post } from "@/mongodb/models/post";
+import { SignedIn } from "@clerk/nextjs";
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+
+  await connectDB();
+  const posts = await Post.getAllPosts();
+
+  console.log(posts);
+
+
   return (
     <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
@@ -9,8 +22,12 @@ export default function Home() {
       </section>
 
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
-     <PostForm />
-    {/* postFeed */}
+
+       <SignedIn>
+        <PostForm />
+       </SignedIn>
+     
+        <PostFeed posts={posts} />
 
       </section>
 
