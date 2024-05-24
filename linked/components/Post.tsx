872247535b17f6fecf2fae82c3b29d3c3f3ff178
wael <78425538+wa20@users.noreply.github.com>
2024-05-24 10:18:46 +0100

    updated post component and added import post options component to it

--- linked/components/Post.tsx
+++ linked/components/Post.tsx
@@ -5,10 +5,12 @@ import { useUser } from "@clerk/nextjs";
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import { Badge } from "./ui/badge";
-import ReactTimeago from 'react-timeago'
+// import ReactTimeago from "react-timeago";
 import { Button } from "./ui/button";
 import { Trash2 } from "lucide-react";
-
+import deletePostAction from "@/actions/deletePostAction";
+import Image from "next/image";
+import PostOptions from "./PostOptions";
 
 function Post({ post }: { post: IPostDocument }) {
   const { user } = useUser();
@@ -18,39 +20,65 @@ function Post({ post }: { post: IPostDocument }) {
   return (
     <div className="bg-white rounded-md border">
       <div className="p-4 flex space-x-2">
-        <Avatar>
-          <AvatarImage src={post.user.userImage} />
-          <AvatarFallback>
-            {post.user.firstName?.charAt(0)}
-            {post.user.lastName?.charAt(0)}
-          </AvatarFallback>
-        </Avatar>
-      </div>
-      <div className="flex justify-between flex-1">
         <div>
-          <p className="font-semibold">
-            {post.user.firstName} {post.user.lastName}{" "}
-            {isAuthor && (
-              <Badge className="ml-2" variant="secondary">
-                Author
-              </Badge>
-            )}
-          </p>
-          <p className="text-xs text-gray-400">
-            @{post.user.firstName}
-            {post.user.firstName}-{post.user.userId.toString().slice(-4)}
-          </p>
-
-          <p className="text-xs text-gray-400">
-            <ReactTimeago date={new Date(post.createdAt)} />
-          </p>
+          <Avatar>
+            <AvatarImage src={post.user.userImage} />
+            <AvatarFallback>
+              {post.user.firstName?.charAt(0)}
+              {post.user.lastName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
         </div>
-        {isAuthor && (
-          <Button>
-            <Trash2 size={20} />
-          </Button>
+
+        <div className="flex justify-between flex-1">
+          <div>
+            <p className="font-semibold">
+              {post.user.firstName} {post.user.lastName}{" "}
+              {isAuthor && (
+                <Badge className="ml-2" variant="secondary">
+                  Author
+                </Badge>
+              )}
+            </p>
+            <p className="text-xs text-gray-400">
+              @{post.user.firstName}
+              {post.user.firstName}-{post.user.userId.toString().slice(-4)}
+            </p>
+
+            <p className="text-xs text-gray-400">
+              {/* <ReactTimeago date={new Date(post.createdAt)} /> */}
+            </p>
+          </div>
+          {isAuthor && (
+            <Button
+              variant="secondary"
+              onClick={() => {
+                const promise = deletePostAction(post._id.toString())
+                // TODO add toast
+              }}
+            >
+              <Trash2 size={20} />
+            </Button>
+          )}
+        </div>
+      </div>
+
+      <div>
+        <p className="px-4 pb-2 mt-2">{post.text}</p>
+        {post.imageUrl && (
+          <Image
+            src={post.imageUrl}
+            alt="Post image"
+            width={500}
+            height={500}
+            className="w-full mx-auto"
+          />
         )}
       </div>
+       <PostOptions post={post} />
+
+
+      {/*  TODO Post options*/}
     </div>
   );
 }
