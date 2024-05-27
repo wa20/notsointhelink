d64eebbf09429f476d98a3216684c7410112ddb1
wael <78425538+wa20@users.noreply.github.com>
2024-05-27 11:01:18 +0100

    updated next config file to whitelist sites

--- linked/next.config.mjs
+++ linked/next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'links.papareact.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'limg.clerk.com',
+          },
+          {
+            protocol: 'https',
+            hostname: 'getlinked.blob.core.windows.net',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
