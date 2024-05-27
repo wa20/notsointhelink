20e8f0ede6fb833a21ec699167cff8dc9f60b00a
wael <78425538+wa20@users.noreply.github.com>
2024-05-27 11:14:05 +0100

    added widget to page and updated userinfo compoennt prop

--- linked/app/page.tsx
+++ linked/app/page.tsx
@@ -1,6 +1,7 @@
 import PostFeed from "@/components/PostFeed";
 import PostForm from "@/components/PostForm";
 import UserInfo from "@/components/UserInfo";
+import Widget from "@/components/Widget";
 import connectDB from "@/mongodb/db";
 import { Post } from "@/mongodb/models/post";
 import { SignedIn } from "@clerk/nextjs";
@@ -8,31 +9,27 @@ import { SignedIn } from "@clerk/nextjs";
 export const revalidate = 0;
 
 export default async function Home() {
-
   await connectDB();
   const posts = await Post.getAllPosts();
 
   console.log(posts);
 
-
   return (
     <div className="grid grid-cols-8 mt-5 sm:px-5">
       <section className="hidden md:inline md:col-span-2">
-        <UserInfo />
+        <UserInfo posts={posts} />
       </section>
 
       <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <SignedIn>
+          <PostForm />
+        </SignedIn>
 
-       <SignedIn>
-        <PostForm />
-       </SignedIn>
-     
         <PostFeed posts={posts} />
-
       </section>
 
       <section className="hidden xl:inline justify-center col-span-2">
-        {/* widget */}
+        <Widget />
       </section>
     </div>
   );
